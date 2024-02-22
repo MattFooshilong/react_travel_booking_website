8ad1855dd8c5e15d4f7b8b8545b0ccff387f44b1
Matt Foo <shilongfoo@gmail.com>
2024-02-22 15:35:39 +0800

    updated footer padding and margins

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,22 +6,22 @@ class Footer extends Component {
         return (
             <>
             <div className="container-fluid bg-danger" id="footer">
-                <Row className="justify-content-center text-white text-left">
-                    <Col lg={3}>
+                <Row className="justify-content-center text-white text-center py-0 py-lg-3">
+                    <Col lg={3} className="mt-4 mt-lg-0">
                         <h2>About Us</h2>
                         <a href="#" class="text-white">Press</a><br />
                         <a href="#" class="text-white">Blog</a><br />
                         <a href="#" class="text-white">Reviews</a><br />
                     </Col>
                         
-                    <Col lg={3} className="mb-4">
+                    <Col lg={3} className="mt-4 mt-lg-0">
                         <h2>Contact Us</h2>
                         <p className="m-0">+65 65137892</p>
-                        <p className="m-0">TravelAsia@travelHoldings.com</p><br />
+                        <p className="m-0">TravelAsia@travelHoldings.com</p>
                         <address className="m-0">8 Chinatown Building A 600123</address>
                         <p className="m-0">Directions: Exit Chinatown MRT via exit A</p>
                     </Col>
-                    <Col lg={3}>
+                    <Col lg={3} className="mt-4 mt-lg-0">
                         <h2>Terms of Use</h2>
                         <a href="#" class="text-white">Privacy Policy</a><br />
                         <a href="#" class="text-white">Terms and Conditions</a>
